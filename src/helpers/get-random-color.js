7554e2596caf4bf93f200dce6bddbbcdb66d8ada
Klypalskyi <klypalskyi@gmail.com>
2019-09-18 22:16:04 +0300

    remove helpers

--- src/helpers/get-random-color.js
+++
@@ -1,8 +0,0 @@
-const generateRandomColor = () => {
-  const r = Math.round(Math.random() * 255); // red 0 to 255
-  const g = Math.round(Math.random() * 255); // green 0 to 255
-  const b = Math.round(Math.random() * 255); // blue 0 to 255
-  return `rgb(${r},${g},${b})`;
-};
-
-export default generateRandomColor;
