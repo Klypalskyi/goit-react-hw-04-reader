b8d5c1aced0cc6efa279748129a99cbc3fdff5aa
Klypalskyi <klypalskyi@gmail.com>
2019-09-18 21:39:23 +0300

    Updates

---
+++ precache-manifest.537704659994a657e50a2510c3f9cd74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f0bf477e49d16eb3d11949d15d92da",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "1b61d505794188b44595",
+    "url": "/goit-react-hw-01-components/static/css/main.a462de12.chunk.css"
+  },
+  {
+    "revision": "49781e6cf5c3eff9c896",
+    "url": "/goit-react-hw-01-components/static/js/2.467bbe50.chunk.js"
+  },
+  {
+    "revision": "1b61d505794188b44595",
+    "url": "/goit-react-hw-01-components/static/js/main.670c6ab0.chunk.js"
+  },
+  {
+    "revision": "3fff2c44a6ade889c1dc",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.83f0207d.js"
+  }
+]);
